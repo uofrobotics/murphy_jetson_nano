93b5342ebe44ac3afc550a2530b01448e8b9e581
mohammad <mohammadalizafar986@gmail.com>
2024-02-21 03:25:07 -0600

    fixed list bug

--- jetson/process_input.py
+++ jetson/process_input.py
@@ -1,27 +1,30 @@
-# process_input.py
 import math
 
 def process_input(data):
-    # Process the received data
-    # For example, convert it to integers for UART communication
-    processed_data = [int(val * 100) for val in data]
-    print("     Processed Data: ", processed_data)
-    curved_data = match_input_to_curve(data)
-    print("     Curved Data: ", curved_data)
-
-    return processed_data
+    processed_data = []
+    curved_data = []
+    
+    # Process each value in the data list
+    for value in data:
+        processed_value = int(value * 100)  # For example, convert it to integers for UART communication
+        processed_data.append(processed_value)
+        
+        # Apply the curve function to each value
+        curved_value = match_input_to_curve(value)
+        curved_data.append(curved_value)
+    
+    print("Processed Data:", processed_data)
+    print("Curved Data:", curved_data)
+    
+    return processed_data, curved_data
 
 def match_input_to_curve(value):
     sensitivity = 10
     offset = 50
-    curved_value = []
 
-    for i in range(value):
-        curved_value[i] = int(128 / (1 + math.exp((-(abs(value[i])) + offset) / sensitivity)))
+    curved_value = int(128 / (1 + math.exp((-(abs(value)) + offset) / sensitivity)))
 
-        if value[i] < 0:
-            curved_value[i] = -curved_value
-
-    
+    if value < 0:
+        curved_value = -curved_value
 
     return curved_value
